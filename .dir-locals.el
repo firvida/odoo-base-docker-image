f24b3ec70cf6e16a9aaf5709955d2baa5ac82098
Manuel Vázquez Acosta <manuel@merchise.org>
2019-03-10 22:18:52 -0400

    Extract common packages to Dockerfile.common

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((dockerfile-mode
+   (indent-tabs-mode)))
